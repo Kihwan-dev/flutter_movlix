f788f7d034316a632db10b15d8ccc0ac9adbcd38
Kihwan-dev <rlrkf2420@gmail.com>
2025-08-24 04:38:50 +0900

    ✨ MovieDataSourceImpl 구현

--- lib/features/movie/data/data_sources/movie_data_source_impl.dart
+++ lib/features/movie/data/data_sources/movie_data_source_impl.dart
@@ -1,35 +1,43 @@
+import 'dart:convert';
+
+import 'package:flutter_movlix/core/constants/api_endpoints.dart';
 import 'package:flutter_movlix/features/movie/data/data_sources/movie_data_source.dart';
 import 'package:flutter_movlix/features/movie/data/dtos/movie_detail_dto.dart';
 import 'package:flutter_movlix/features/movie/data/dtos/movie_response_dto.dart';
+import 'package:flutter_movlix/infrastructure/network/dio_client.dart';
 
 class MovieDataSourceImpl implements MovieDataSource {
-  @override
-  Future<MovieDetailDto?> fetchMovieDetail(int id) {
-    // TODO: implement fetchMovieDetail
-    throw UnimplementedError();
+
+  Future<MovieResponseDto?> fetch(String endpoint) async {
+    final response = await DioClient.client.get(endpoint);
+    final json = jsonDecode(response.toString());
+    return MovieResponseDto.fromJson(json);
   }
 
   @override
-  Future<MovieResponseDto?> fetchNowPlayingMovies() {
-    // TODO: implement fetchNowPlayingMovies
-    throw UnimplementedError();
+  Future<MovieResponseDto?> fetchNowPlayingMovies() async {
+    return fetch(ApiEndpoints.nowPlaying);
   }
 
   @override
-  Future<MovieResponseDto?> fetchPopularMovies() {
-    // TODO: implement fetchPopularMovies
-    throw UnimplementedError();
+  Future<MovieResponseDto?> fetchPopularMovies() async {
+    return fetch(ApiEndpoints.popular);
   }
 
   @override
-  Future<MovieResponseDto?> fetchTopRatedMovies() {
-    // TODO: implement fetchTopRatedMovies
-    throw UnimplementedError();
+  Future<MovieResponseDto?> fetchTopRatedMovies() async {
+    return fetch(ApiEndpoints.topRated);
   }
 
   @override
-  Future<MovieResponseDto?> fetchUpcomingMovies() {
-    // TODO: implement fetchUpcomingMovies
-    throw UnimplementedError();
+  Future<MovieResponseDto?> fetchUpcomingMovies() async {
+    return fetch(ApiEndpoints.upcoming);
+  }
+
+  @override
+  Future<MovieDetailDto?> fetchMovieDetail(int id) async {
+    final response = await DioClient.client.get(ApiEndpoints.detail(id));
+    final json = jsonDecode(response.toString());
+    return MovieDetailDto.fromJson(json);
   }
 }
