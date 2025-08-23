008c6e8d4f96f61ad86524bce0fb44af1f6ddeb7
Kihwan-dev <rlrkf2420@gmail.com>
2025-08-24 04:21:58 +0900

    ✨ 공통으로 사용할 수 있는 ApiEndpoints 추가

---
+++ lib/core/constants/api_endpoints.dart
@@ -0,0 +1,8 @@
+class ApiEndpoints {
+  static const nowPlaying = "now_playing";
+  static const popular = "now_playing";
+  static const topRated = "now_playing";
+  static const upcoming = "now_playing";
+
+  static String detail(int id) => "$id";
+}
